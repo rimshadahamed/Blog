98773b0cb47b8dd6f0232fc15b8c5a30da392ad8
Rimshad <rimshad.rimo@gmail.com>
2018-05-03 21:04:28 +0530

    composer-update

--- config/database.php
+++ config/database.php
@@ -41,7 +41,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '46.101.151.212'),
+            'host' => env('DB_HOST', 'localhost'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'jafczxajrn'),
             'username' => env('DB_USERNAME', 'jafczxajrn'),
