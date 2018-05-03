34cdf8927d7baadd288ed470db3771880537926f
Rimshad <rimshad.rimo@gmail.com>
2018-05-03 20:36:50 +0530

    db-update

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '46.101.151.212'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'jafczxajrn'),
+            'username' => env('DB_USERNAME', 'jafczxajrn'),
+            'password' => env('DB_PASSWORD', '5HDBecYrBP'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
